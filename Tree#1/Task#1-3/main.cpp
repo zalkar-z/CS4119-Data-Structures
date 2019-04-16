f11791f5003b90ed67de2cc0d20def698f884819
Zalkar <zalkar@bennington.edu>
2019-04-16 15:43:20 -0400

    Lab Tree#1 is done

--- Tree#1/Task#1-3/main.cpp
+++ Tree#1/Task#1-3/main.cpp
@@ -11,10 +11,14 @@
 using namespace std;
 
 int factorial(int);
+void readFile(string);
 
 int main() {
 
     cout << factorial(4) << endl;
+
+    string filePath = "C:\\Users\\User\\Desktop\\Bennington College\\term2\\Data_Structures_C++\\Tree#1\\Task#1-3\\test.txt";
+    readFile(filePath);
 
     return 0;
 }
@@ -22,4 +26,17 @@ int main() {
 int factorial(int n) {
     if (n == 1) return 1;
     return n * factorial(n - 1);
+}
+
+void readFile(string fileName) {
+    ifstream myFile(fileName);
+    string word;
+
+    while(myFile>>word) {
+        // the whole word
+        cout << word << endl;
+        // the first char of string
+        cout << word.at(0) << endl;
+    }
+    myFile.close();
 }
